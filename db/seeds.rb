b52b8e9d8e1ee9ec985ada64acf3c7ada866a309
Brendan Hamill <brenhamill@gmail.com>
2018-01-06 17:25:28 -0500

    adds seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-# User.destroy_all
-# Book.destroy_all
-#
-# User.create(username: "user1", password: "starwars")
-#
-# Book.create(title:"The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/11.jpg", description: "Seconds before Earth is demolished to make way for a galactic freeway, Arthur Dent is plucked off the planet by his friend Ford Prefect, a researcher for the revised edition of The Hitchhiker’s Guide to the Galaxy who, for the last fifteen years, has been posing as an out-of-work actor.")
-#
-# Book.create(title:"Perdido Street Station", author: "China Miéville", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/perdido.jpg", description: "Beneath the towering bleached ribs of a dead, ancient beast lies the city of New Crobuzon, where the unsavory deal is stranger to no one--not even to Isaac, a gifted and eccentric scientist who has spent a lifetime quietly carrying out his unique research. ")
-#
-# Book.create(title:"1491: New Revelations of the Americas Before Columbus", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/1491.jpg", author: "Charles C. Mann", description: "A groundbreaking study that radically alters our understanding of the Americas before the arrival of the Europeans in 1492.
-# ")
-# Book.create(title:"Persepolis: The Story of a Childhood", author: "Marjane Satrapi", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/persepolis.jpg", description: "Wise, funny, and heartbreaking, Persepolis is Marjane Satrapi’s memoir of growing up in Iran during the Islamic Revolution.")
+User.destroy_all
+Book.destroy_all
 
-# Book.create(title:"Foundation", author: "Isaac Asimov", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/foundation.jpg", description: "For twelve thousand years the Galactic Empire has ruled supreme. Now it is dying. But only Hari Seldon, creator of the revolutionary science of psychohistory, can see into the future -- to a dark age of ignorance, barbarism, and warfare that will last thirty thousand years. To preserve knowledge and save mankind, Seldon gathers the best minds in the Empire -- both scientists and scholars -- and brings them to a bleak planet at the edge of the Galaxy to serve as a beacon of hope for a future generations. He calls his sanctuary the Foundation.
-# ")
-#
-# 50.times do
-#   Book.create(title: Faker::Book.title, author: Faker::Book.author, description: Faker::Dune.quote)
-# end
-#
-# ShelfMembership.create(shelf_id: 1, book_id: 1)
-# ShelfMembership.create(shelf_id: 1, book_id: 2)
-# ShelfMembership.create(shelf_id: 1, book_id: 3)
-# ShelfMembership.create(shelf_id: 1, book_id: 4)
-# ShelfMembership.create(shelf_id: 2, book_id: 5)
-# ShelfMembership.create(shelf_id: 2, book_id: 6)
-# ShelfMembership.create(shelf_id: 2, book_id: 7)
-# ShelfMembership.create(shelf_id: 3, book_id: 9)
-# ShelfMembership.create(shelf_id: 2, book_id: 8)
-# ShelfMembership.create(shelf_id: 1, book_id: 10)
-# ShelfMembership.create(shelf_id: 1, book_id: 11)
+User.create(username: "user1", password: "starwars")
+
+Book.create(title:"The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/11.jpg", description: "Seconds before Earth is demolished to make way for a galactic freeway, Arthur Dent is plucked off the planet by his friend Ford Prefect, a researcher for the revised edition of The Hitchhiker’s Guide to the Galaxy who, for the last fifteen years, has been posing as an out-of-work actor.")
+
+Book.create(title:"Perdido Street Station", author: "China Miéville", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/perdido.jpg", description: "Beneath the towering bleached ribs of a dead, ancient beast lies the city of New Crobuzon, where the unsavory deal is stranger to no one--not even to Isaac, a gifted and eccentric scientist who has spent a lifetime quietly carrying out his unique research. ")
+
+Book.create(title:"1491: New Revelations of the Americas Before Columbus", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/1491.jpg", author: "Charles C. Mann", description: "A groundbreaking study that radically alters our understanding of the Americas before the arrival of the Europeans in 1492.
+")
+Book.create(title:"Persepolis: The Story of a Childhood", author: "Marjane Satrapi", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/persepolis.jpg", description: "Wise, funny, and heartbreaking, Persepolis is Marjane Satrapi’s memoir of growing up in Iran during the Islamic Revolution.")
+
+Book.create(title:"Foundation", author: "Isaac Asimov", cover_image: "https://s3.us-east-2.amazonaws.com/shelf-imgs/foundation.jpg", description: "For twelve thousand years the Galactic Empire has ruled supreme. Now it is dying. But only Hari Seldon, creator of the revolutionary science of psychohistory, can see into the future -- to a dark age of ignorance, barbarism, and warfare that will last thirty thousand years. To preserve knowledge and save mankind, Seldon gathers the best minds in the Empire -- both scientists and scholars -- and brings them to a bleak planet at the edge of the Galaxy to serve as a beacon of hope for a future generations. He calls his sanctuary the Foundation.
+")
+
+50.times do
+  Book.create(title: Faker::Book.title, author: Faker::Book.author, description: Faker::Dune.quote)
+end
