7bc26ac5fc945648b07fb994759c851c9203819a
Brendan Hamill <brenhamill@gmail.com>
2018-01-08 18:37:21 -0500

    ends long hover rating nightmare

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Footer = (props) => {
   return (
-    <ul className="footer-links">
-      <li><a href="https://github.com/Bhammy">Github</a></li>
-      <li><a href="">Portfolio</a></li>
-      <li id="last-footer-link"><a href="mailto: brenhamill@gmail.com">Email</a></li>
-    </ul>
+      <ul className="footer-links">
+        <li><a href="https://github.com/Bhammy">Github</a></li>
+        <li><a href="">Portfolio</a></li>
+        <li id="last-footer-link"><a href="mailto: brenhamill@gmail.com">Email</a></li>
+      </ul>
   );
 };
 
 export default Footer;
+
+// <div><h6><i>Icons made by <a href="https://www.flaticon.com/authors/zlatko-najdenovski" title="Zlatko Najdenovski">Zlatko Najdenovski</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></i></h6></div>
--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -10,6 +10,7 @@ class ReviewForm extends React.Component {
     } else {
       this.formType = "edit";
     }
+    this.setRating = this.setRating.bind(this);
   }
 
   handleChange(field) {
@@ -42,6 +43,10 @@ class ReviewForm extends React.Component {
     $("textarea").prop('disabled', false);
   }
 
+  setRating (e) {
+    this.setState({ rating: e.target.value });
+  }
+
   render(){
 
     let editButton = () => (
@@ -56,7 +61,50 @@ class ReviewForm extends React.Component {
       <div className="review-form">
         <label><h3><i>Leave a Review!</i></h3>
           <form>
-            <label>Review Title
+            <div className="review-form-rating">
+
+              <input type="radio" value={5} name="rating"
+                onClick={ this.setRating }
+                className="rating-input"
+                id="rating-input-5"
+                defaultChecked={ (this.state.rating === 5 ) } />
+            <label className="rating-img" htmlFor="rating-input-5">
+            </label>
+
+              <input type="radio" value={4} name="rating"
+                onClick={ this.setRating }
+                className="rating-input"
+                id="rating-input-4"
+                defaultChecked={ (this.state.rating === 4 ) } />
+            <label className="rating-img" htmlFor="rating-input-4">
+            </label>
+
+              <input type="radio" value={3} name="rating"
+                onClick={ this.setRating }
+                className="rating-input"
+                id="rating-input-3"
+                defaultChecked={ (this.state.rating === 3 ) } />
+            <label className="rating-img" htmlFor="rating-input-3">
+            </label>
+
+              <input type="radio" value={2} name="rating"
+                onClick={ this.setRating }
+                className="rating-input"
+                id="rating-input-2"
+                defaultChecked={ (this.state.rating === 2 ) } />
+            <label className="rating-img" htmlFor="rating-input-2">
+            </label>
+
+              <input type="radio" value={1} name="rating"
+                onClick={ this.setRating }
+                className="rating-input"
+                id="rating-input-1"
+                defaultChecked={ (this.state.rating === 1 ) } />
+            <label className="rating-img" htmlFor="rating-input-1">
+            </label>
+
+            </div>
+          <label>Review Title
               <br />
               <input type="text" value={ this.state.title } onChange={ this.handleChange("title") } disabled={ (this.formType === "create" ? false : true)} />
             </label>
